961f62d3621c846df2e5386f5924e6453117d4a8
Prashant Dhungana <prashantsharma.dhungana@texasintl.edu.np>
2021-07-02 23:33:04 +0545

    [ADDED] Popup when extension btn is clicked

--- content.js
+++ content.js
@@ -1,12 +1,22 @@
 let paragraphs = document.getElementsByTagName('p');
+let images = document.getElementsByTagName('img');
 
-
-chrome.runtime.onMessage.addListener(gotMessage);
+chrome.runtime.onMessage.addListener(imageChange);
 
 function gotMessage(message, sender, sendResponse){
 	if(message.txt == "Namastey"){
 		for(x of paragraphs){
 			x.style['background-color'] = "#FF6633";
+		}
+	}
+}
+
+function imageChange(message, sender, sendResponse){
+	if(message.txt == "Namastey"){
+		for(img of images){
+			let url = chrome.extension.getURL("images/code.jpg");
+			img.src = url;
+			console.log(url);
 		}
 	}
 }
